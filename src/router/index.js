6e4b702443f9831e5725f25674944f606cd6ccf9
Eric Yu <yyyu@deakin.edu.au>
2021-09-15 12:04:25 +1000

    First commit

---
+++ src/router/index.js
@@ -0,0 +1,23 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Home from '../views/Home.vue'
+import Mycar from '../views/Mycar.vue'
+Vue.use(VueRouter)
+
+const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: Home
+  },{
+    path:'/mycar',
+    name:'Mycar',
+    component:Mycar
+  }
+]
+
+const router = new VueRouter({
+  routes
+})
+
+export default router
